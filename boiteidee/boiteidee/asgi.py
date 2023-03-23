a95b9e144a5a032e562acba54085f2df7765d2bb
Ingabire Yvette <yvette.ingabire@live.fr>
2023-03-23 13:03:02 +0100

    initialisatiodjango

---
+++ boiteidee/boiteidee/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for boiteidee project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boiteidee.settings')
+
+application = get_asgi_application()
